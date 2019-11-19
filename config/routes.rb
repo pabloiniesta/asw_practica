1e349b2ddbdb9203091aa7acb77dfa51f30b5076
Ubuntu <ubuntu@ip-172-31-5-95.us-east-2.compute.internal>
2019-11-19 16:31:20 +0000

    login + google funcionando

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,5 @@ Rails.application.routes.draw do
 root 'sessions#new'
   resources :issues
   resources :users
-  resources :sessions, only: [:new, :create, :destroy]  
-  
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :sessions
 end
